28f7ce642c43a7b96bdf5a7d55a6a354fe6a51cd
Alyss Flynn <alyss.flynn@me.com>
2018-05-26 19:59:31 -0500

    added files

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API keys
+consumer_key = "DeerinPN5Pte6nRq90WvoeEd9"
+consumer_secret = "SkhJsp3nJ80iMRJRBoixyo2xbDRGqyTAVeSr7NGZtg0PLxCLID"
+access_token= "999444856920518658-VXgFP0hYO9i9WXZl7Jhk3CvvsgY7neI"
+access_token_secret = "kj0Fkt9a1gSt8QqvA7OTdRQNNAc1NIukrHvyasIcX3hvu"
